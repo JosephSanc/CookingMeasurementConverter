9eb8dbff8625058c9e1b1123f0005d592620650a
JosephSanc <josephdsanchez@outlook.com>
2020-07-09 10:39:42 -0400

    UserInterface planned and made Teaspoons file

---
+++ src/VolumeConversions/Teaspoons.java
@@ -0,0 +1,89 @@
+package VolumeConversions;
+
+/*
+This program will convert teaspoons to:
+    - Grams
+    - Milligrams
+    - Kilograms
+    - Milliliters
+    - Tablespoons
+    - Fluid ounces
+    - Ounces
+
+    Author: Joseph Sanchez
+    Date: 07/01/2020
+ */
+
+public class Teaspoons {
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in grams
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to grams
+     */
+    public double teaspoonsToGrams(double teaspoons){
+        return teaspoons * 4.92892159;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in milligrams
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to milligrams
+     */
+    public double teaspoonsToMilligrams(double teaspoons){
+        return teaspoons * 5687.50;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in kilograms
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to kilograms
+     */
+    public double teaspoonsToKilograms(double teaspoons){
+        return teaspoons * 0.004928922;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in milliliters
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to milliliters
+     */
+    public double teaspoonsToMilliliters(double teaspoons){
+        return teaspoons * 4.92892;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in tablespoons
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to tablespoons
+     */
+    public double teaspooonsToTablespoons(double teaspoons){
+        return teaspoons * 0.333333;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in fluid ounces
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to fluid ounces
+     */
+    public double teaspoonsToFluidOunces(double teaspoons){
+        return teaspoons * 0.166667;
+    }
+
+    /**
+     * Description: This method will take an "teaspoons" argument and output its value in ounces
+     *
+     * @param teaspoons the amount of ounces we are trying to convert
+     * @return the value of teaspoons when it is converter to ounces
+     */
+    public double teaspoonsToOunces(double teaspoons){
+        return teaspoons * 0.166667;
+    }
+
+}
--- src/WeightConversions/Milligrams.java
+++ src/WeightConversions/Milligrams.java
@@ -13,19 +13,43 @@ In this class I will convert milligrams to:
 
 public class Milligrams {
 
+    /**
+     * Description: This method will take an "milligrams" argument and output its value in ounces
+     *
+     * @param milligrams the amount of milligrams we are trying to convert
+     * @return the value of milligrams when it is converter to ounces
+     */
     public double milligramsToOunces(double milligrams){
         return milligrams * 0.000035274;
     }
 
+    /**
+     * Description: This method will take an "milligrams" argument and output its value in grams
+     *
+     * @param milligrams the amount of milligrams we are trying to convert
+     * @return the value of milligrams when it is converter to grams
+     */
     public double milligramsToGrams(double milligrams){
         return milligrams / 1000;
     }
 
+    /**
+     * Description: This method will take an "milligrams" argument and output its value in kilograms
+     *
+     * @param milligrams the amount of milligrams we are trying to convert
+     * @return the value of milligrams when it is converter to kilograms
+     */
     public double milligramsToKilograms(double milligrams){
         return milligrams / 1000000;
     }
 
-    public double milligramsToPounds(double milligrams){
+    /**
+     * Description: This method will take an "milligrams" argument and output its value in pounds
+     *
+     * @param milligrams the amount of milligrams we are trying to convert
+     * @return the value of milligrams when it is converter to pounds
+     */
+    public double milligramsToPounds(double milligrams){ //this method isn't very useful unless the milligrams are large
         return milligrams * 0.0000022046;
     }
 
